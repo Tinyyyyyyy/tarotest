8ea48cd0b2d9311de058edaaa21d88a2076cc4cc
jianbo.ye <yejianbo03151@polyic.cn>
2022-06-25 11:37:21 +0800

    init

---
+++ src/comps/test/index.tsx
@@ -0,0 +1,23 @@
+import { Component } from "react";
+import { View, Text } from "@tarojs/components";
+import "./style.less";
+
+export default class Index extends Component {
+  componentWillMount() {}
+
+  componentDidMount() {}
+
+  componentWillUnmount() {}
+
+  componentDidShow() {}
+
+  componentDidHide() {}
+
+  render() {
+    return (
+      <View className="test">
+        <Text>test</Text>
+      </View>
+    );
+  }
+}
